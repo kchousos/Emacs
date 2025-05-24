f224b64ec1ececdbc0ad5d314974745d7c9a1647
Konstantinos Chousos <K.chousos@protonmail.com>
2025-05-24 23:57:12 +0300

    use-package integration and zk package

--- init.el
+++ init.el
@@ -1,40 +1,42 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Package system setup
-;; Must come before any package use
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;; Package System & use-package Setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu"   . "https://elpa.gnu.org/packages/")))
+(package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; Miscellaneous settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Miscellaneous Settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; The default is 800 kilobytes. Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
-;; Profile emacs startup
 (add-hook 'emacs-startup-hook
           (lambda ()
             (message "*** Emacs loaded in %s seconds with %d garbage collections."
                      (emacs-init-time "%.2f")
                      gcs-done)))
 
-;; Silence compiler warnings as they can be pretty disruptive
 (setq native-comp-async-report-warnings-errors nil)
-
-;; Set the right directory to store the native comp cache
 (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
 
 (setq inhibit-startup-message t)
-
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
 (setq delete-by-moving-to-trash t)
 
 (tool-bar-mode -1)
@@ -46,23 +48,13 @@
 (set-default-coding-systems 'utf-8)
 
 (setq x-select-enable-clipboard t)
-
-;; use `view-mode` for read-only files
 (setq view-read-only t)
-
 (setq ring-bell-function #'ignore)
-
 (defalias 'yes-or-no-p 'y-or-n-p)
-
 (setq column-number-mode t)
-
-;; Remove the line-wrap fringe indicators
 (setq-default fringe-indicator-alist nil)
-
 (fringe-mode 0)
-
 (blink-cursor-mode 1)
-
 (global-hl-line-mode 0)
 
 (global-set-key (kbd "M-<f3>") 'scroll-bar-mode)
@@ -70,73 +62,64 @@
 (window-divider-mode 0)
 
 (setq scroll-bar-adjust-thumb-portion t)
-
 (set-window-scroll-bars (minibuffer-window) nil nil nil nil t)
 
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-
-(setq scroll-step 1) ;; keyboard scroll one line at a time
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 1)
 
 (pixel-scroll-precision-mode t)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil
+              tab-width 4)
 (setq indent-line-function #'insert-tab)
 
-(setq electric-pair-pairs '(
-                            (?\{ . ?\})
-                            (?\( . ?\))
-                            (?\[ . ?\])
-                            (?\" . ?\")
-                            ))
+(setq electric-pair-pairs '((?\{ . ?\}) (?\( . ?\)) (?\[ . ?\]) (?\" . ?\")))
 (electric-pair-mode t)
 
 (global-display-line-numbers-mode 0)
-
 (setq display-line-numbers-type 'relative)
-
 (setq warning-minimum-level :emergency)
 
 (recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 25)
+(setq recentf-max-menu-items 25
+      recentf-max-saved-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 (run-at-time nil (* 5 60) 'recentf-save-list)
 
 (setq isearch-lazy-count t)
-
 (setq auto-revert-avoid-polling t)
 (global-auto-revert-mode)
 
 (setq bookmark-save-flag 1)
-
 (delete-selection-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fonts
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun set-font-faces ()
-        (message "Setting faces!")
-        (set-fontset-font t 'symbol (font-spec :family "Noto Color Emoji" :size 24))
-        (set-face-attribute 'default nil :family "Iosevka" :height 120)
-        (set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 1.0)
-        (set-face-attribute 'variable-pitch nil :family "Inter"))
+  (message "Setting faces!")
+  (set-fontset-font t 'symbol (font-spec :family "Noto Color Emoji" :size 24))
+  (set-face-attribute 'default nil :family "Iosevka" :height 120)
+  (set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 1.0)
+  (set-face-attribute 'variable-pitch nil :family "Lato" :height 1.0))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
               (lambda (frame)
                 (with-selected-frame frame
                   (set-font-faces))))
-    (set-font-faces))
+  (set-font-faces))
 
 (setq line-spacing 0.0)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Keybinds
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (global-set-key (kbd "M-<f2>") #'(lambda () (interactive) (display-line-numbers-mode #'toggle)))
 
@@ -145,7 +128,6 @@
   (split-window-right)
   (balance-windows)
   (other-window 1))
-
 (global-set-key (kbd "C-x 3") #'split-and-follow-vertically)
 
 (defun split-and-follow-horizontally ()
@@ -153,7 +135,6 @@
   (split-window-below)
   (balance-windows)
   (other-window 1))
-
 (global-set-key (kbd "C-x 2") #'split-and-follow-horizontally)
 
 (add-hook 'after-init-hook (lambda () (setq default-input-method "greek")))
@@ -176,46 +157,27 @@
         (kill-buffer (current-buffer))))))
 (global-set-key (kbd "C-x C-k") #'my-kill-this-buffer)
 
-;; Show syntax highlighting
-(global-font-lock-mode t)
-
-(require 'tree-sitter)
-(require 'tree-sitter-langs)
-(global-tree-sitter-mode)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'which-key)
-(which-key-mode)
+(use-package which-key
+  :config (which-key-mode))
 
-(defvar mine:dark-theme 'modus-vivendi
-  "Default dark theme.")
+(use-package modus-themes)
 
-(defvar mine:light-theme 'modus-operandi
-  "Default light theme.")
+(defvar mine:dark-theme 'modus-vivendi)
+(defvar mine:light-theme 'modus-operandi)
 
 (defun mine:theme-from-dbus (value)
-  "Change the theme based on a D-Bus property.
-
-VALUE should be an integer or an arbitrarily nested list that
-contains an integer.  When VALUE is equal to 2 then a light theme
-will be selected, otherwise a dark theme will be selected."
   (load-theme (if (= 2 (car (flatten-list value)))
                   mine:light-theme
                 mine:dark-theme)
               t))
 
-(setq modus-themes-headings
-      '((1 . (1.8))
-        (2 . (1.6))
-        (3 . (1.4))
-        (4 . (1.2))
-        (t . (1.0))))
-
-(require 'dbus)
-;; Set the current theme based on what the system theme is right now:
-(dbus-call-method-asynchronously
+(use-package dbus
+  :config
+  (dbus-call-method-asynchronously
    :session "org.freedesktop.portal.Desktop"
    "/org/freedesktop/portal/desktop"
    "org.freedesktop.portal.Settings"
@@ -223,117 +185,91 @@ will be selected, otherwise a dark theme will be selected."
    #'mine:theme-from-dbus
    "org.freedesktop.appearance"
    "color-scheme")
-;; Register to be notified when the system theme changes:
-(dbus-register-signal
+  (dbus-register-signal
    :session "org.freedesktop.portal.Desktop"
    "/org/freedesktop/portal/desktop"
    "org.freedesktop.portal.Settings"
    "SettingChanged"
-   (lambda (path var value)
-     (when (and (string-equal path "org.freedesktop.appearance")
-                (string-equal var "color-scheme"))
-       (mine:theme-from-dbus value))))
+   (lambda (_path var value)
+     (when (string-equal var "color-scheme")
+       (mine:theme-from-dbus value)))))
 
-(require 'undo-tree)
-(setq undo-tree-auto-save-history t)
+(use-package vundo
+  :bind ("C-x C-u" . vundo)
+  :config
+  (setq vundo-glyph-alist vundo-unicode-symbols
+        vundo-window-max-height 5))
 
-(defadvice undo-tree-make-history-save-file-name
-    (after undo-tree activate)
-  (setq ad-return-value (concat ad-return-value ".gz")))
+(use-package dashboard
+  :config
+  (setq dashboard-startup-banner 'logo
+        dashboard-center-content t
+        dashboard-vertically-center-content t
+        dashboard-startupify-list '(dashboard-insert-banner))
+  (dashboard-setup-startup-hook)
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))))
 
-(setq undo-tree-visualizer-diff t)
-(setq undo-tree-history-directory-alist '(("." . "~/.config/emacs/undo")))
+(use-package project
+  :config
+  (setq project-vc-extra-root-markers '(".project")))
 
-;; Create the directory if it doesn't exist
-(unless (file-exists-p "~/.config/emacs/undo")
-  (make-directory "~/.config/emacs/undo" t))
+(use-package vertico
+  :config (vertico-mode))
 
-(global-undo-tree-mode)
+(use-package orderless
+  :config
+  (setq completion-styles '(orderless)))
 
-(require 'dashboard)
-;; Set the banner
-(setq dashboard-startup-banner 'logo)
-;; Value can be
-;; 'official which displays the official emacs logo
-;; 'logo which displays an alternative emacs logo
-;; 1, 2 or 3 which displays one of the text banners
-;; "path/to/your/image.png" or "path/to/your/text.txt" which displays whatever image/text you would prefer
-;; Content is not centered by default. To center, set
-(setq dashboard-center-content t)
-(setq dashboard-vertically-center-content t)
-;; (setq dashboard-set-navigator nil)
-;; (setq dashboard-banner-logo-title nil)
-;; (setq dashboard-show-shortcuts nil)
-;; (setq dashboard-set-heading-icons nil)
-;; (setq dashboard-set-file-icons nil)
-;; (setq dashboard-set-init-info nil)
-;; (setq dashboard-set-footer nil)
-;; (setq dashboard-week-agenda nil)
-;; (setq dashboard-page-separator "\n\n")
-;; (setq dashboard-items nil)
-(setq dashboard-startupify-list '(dashboard-insert-banner
-                                  ;; dashboard-insert-newline
-                                  ;; dashboard-insert-banner-title
-                                  ;; dashboard-insert-newline
-                                  ;; dashboard-insert-init-info
-                                  ;; dashboard-insert-items
-                                  ;; dashboard-insert-newline
-                                  ;; dashboard-insert-footer
-                                  ))
+(use-package evil
+  :config (evil-mode 1))
 
-(dashboard-setup-startup-hook)
-(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+(use-package markdown-mode
+  :mode "\\.md\\'"
+  :init
+  (setq-default markdown-enable-math t
+                markdown-fontify-code-blocks-natively t
+                markdown-enable-highlighting-syntax t
+                markdown-enable-wiki-links t
+                markdown-wiki-link-alias-first nil))
 
-;; Projects
-(setq project-vc-extra-root-markers '(".project"))
-
-;; Mini-buffer
-(require 'vertico)
-(vertico-mode)
-
-(require 'orderless)
-(setq completion-styles '(orderless))
-
-;; Vim bindings
-(require 'evil)
-(evil-mode 1)
-
-;; Markdown
-(require 'markdown-mode)
-(setq-default markdown-enable-math t
-              markdown-fontify-code-blocks-natively t
-              markdown-enable-highlighting-syntax t
-              markdown-enable-wiki-links t
-              markdown-wiki-link-alias-first nil)
-
-(require 'visual-fill-column)
-(setq-default visual-fill-column-center-text t
-              visual-fill-column-width 120)
+(use-package visual-fill-column
+  :init
+  (setq-default visual-fill-column-center-text t
+                visual-fill-column-width 120))
 
 (defun my-prose-setup ()
-  "Enable visual line wrapping and visual fill column for prose."
-  (visual-line-mode 1)                         ;; Soft wraps at word boundaries
-  (visual-fill-column-mode 1)                 ;; Visually wrap at fill-column
-  (setq truncate-lines nil))                  ;; Ensure lines are not truncated
-
+  (visual-line-mode 1)
+  (visual-fill-column-mode 1)
+  (setq truncate-lines nil))
 (add-hook 'text-mode-hook #'my-prose-setup)
 
-;; Prog mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (setq-default fill-column 80)
 
-;;----------------------------------------------------------------------------
+(use-package tree-sitter
+  :config (global-tree-sitter-mode))
+
+(use-package tree-sitter-langs)
+
+(use-package zk
+  :custom
+  (zk-directory "~/Documents/Notes/Slipbox")
+  (zk-file-extension "md")
+  (zk-id-time-string-format "%Y%m%d%H%M%S")
+  (zk-id-regexp "\\([0-9]\\{14\\}\\)")
+  :config
+  (zk-setup-auto-link-buttons))
+
+(global-font-lock-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(cdlatex dashboard edit-indirect evil mixed-pitch orderless quarto-mode
-             tree-sitter-langs undo-tree vertico visual-fill-column xenops yaml
-             yaml-mode))
- '(package-vc-selected-packages '((org-mode :url "https://code.tecosaur.net/tec/org-mode"))))
+   '(cdlatex dashboard edit-indirect evil markdown-mode mixed-pitch modus-themes
+             orderless tree-sitter-langs undo-tree vertico visual-fill-column
+             vundo zk)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
