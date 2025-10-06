34ac7afdf32bcbe832b6def8fe4c374fc746704d
Konstantinos Chousos <K.chousos@protonmail.com>
2025-10-06 20:01:34 +0300

    variable pitch font height spec

--- init.el
+++ init.el
@@ -212,7 +212,7 @@
     (set-face-attribute 'default nil :family "Iosevka" :height 110)
     (set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 110))
   (when (find-font (font-spec :name "Iosevka Aile"))
-    (set-face-attribute 'variable-pitch nil :family "Iosevka Aile")))
+    (set-face-attribute 'variable-pitch nil :family "Iosevka Aile" :height 110)))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
